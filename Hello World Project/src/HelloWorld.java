a02d49e0bace13a0529914a0070440dc10af54ca
APCS2nd <APCS2nd@MHS-LT9-19>
2019-08-21 12:17:07 -0700

    complete Hello World Project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//Marina Wong
+//Aug 16, 2019
+//this program introduces java to me
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
