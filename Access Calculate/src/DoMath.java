c71efd3c3babfbcbe77daba43a41f5204a8b3f97
APCS2nd <APCS2nd@MHS-LT9-19>
2019-09-04 13:27:02 -0700

    Part 1 thru first average works

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,17 @@
+//Marina Wong
+//Aug 29, 2019
+//period 4
+//Call math methods from calculate library
+//Client code or runner code
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-11));
+		System.out.println(Calculate.cube(3));
+		System.out.println(Calculate.average(12,2));
+
+	}
+
+}
+
+
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,27 @@
+//Marina Wong
+//Aug 29, 2019
+//period 4
+//group of methods that perform math calculations (a library)
+public class Calculate {
+//square	
+	public static int square(int number) {
+		int answer = 0;
+		answer = number* number;
+		return answer;
+	}
+//cube	
+	public static int cube(int number) {
+		int answer = 0;
+		answer = number* number * number;
+		return answer;
+	}
+//average
+		public static double average(double number1, double number2) {
+			double answer = 0;
+			answer = (number1+number2)/2;
+			return answer;
+	}
+
+}
+
+
