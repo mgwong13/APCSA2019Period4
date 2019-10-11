2614e95f353711f0aaa637de9d9c2a5e7d8d97c7
APCS2nd <APCS2nd@MHS-LT9-19>
2019-10-11 12:11:06 -0700

    Processing num in pregress

--- Pair Programing/src/ProcessingNumbers.java
+++ Pair Programing/src/ProcessingNumbers.java
@@ -6,28 +6,48 @@ public class ProcessingNumbers {
 		System.out.println("This program proccesses numbers for you!");
 		System.out.print("How many numbers: ");
 		int totalNumbers = scan.nextInt();
-		System.out.print("What is the minimum (can put any number): ");
-		int max = scan.nextInt();
-		System.out.print("What is the maximum(can put any number): ");
-		int min = scan. nextInt();
+		
 		int sum = 0;
+		int max = -1000000000;
+		int evenmax = -100000000;
+		int min = 100000000;
+		boolean evenfound = false;
 		for(int i=1; i<=totalNumbers; i++) {
+			
 			System.out.print("#"+i+":");
 			int scannedNum = scan.nextInt();
 			if(scannedNum > max) {
-				max = scannedNum;
+				max = scannedNum;			
 			}
 			if(scannedNum < min) {
 				min = scannedNum;
+			}		
+			if(scannedNum % 2 == 0) {
+				evenfound = true;
 			}
-			if(scannedNum %2 ==0) {
-				sum+=scannedNum;
+			if(evenfound = true) {
+				if(scannedNum %2 == 0) {
+					if(scannedNum > max) {
+						evenmax = scannedNum;
+					}
+				}
+				if(scannedNum %2 ==0) {
+					sum+=scannedNum;
+				}
+			if(evenfound = false) {
+				sum = 0;
+			}
+			if(evenfound = false) {
+				evenmax = 0;
 			}
 		}
 		System.out.println("max: "+max+" min: "+ min);
+		System.out.println("max of evens: " + evenmax);
 		System.out.println("Sum of even #s: "+sum);
+		}
+	}
+}
 			
 	
-	}
 
-}
+
