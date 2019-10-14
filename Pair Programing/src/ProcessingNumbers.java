b213b72acf78af81b776dec0017f99a34c0cb41f
APCS2nd <APCS2nd@MHS-LT9-19>
2019-10-14 11:23:17 -0700

    finished pair programing

--- Pair Programing/src/ProcessingNumbers.java
+++ Pair Programing/src/ProcessingNumbers.java
@@ -6,14 +6,19 @@ public class ProcessingNumbers {
 		System.out.println("This program proccesses numbers for you!");
 		System.out.print("How many numbers: ");
 		int totalNumbers = scan.nextInt();
-		
+		System.out.println("What is your first value: ");
+		int value = scan.nextInt();
 		int sum = 0;
-		int max = -1000000000;
-		int evenmax = -100000000;
-		int min = 100000000;
+		int max = value;
+		int evenmax = value;
+		int min = value ;
 		boolean evenfound = false;
-		for(int i=1; i<=totalNumbers; i++) {
+		
+		if (value % 2 == 0) {
+			evenfound = true;
+		}
 			
+		for(int i=2; i<=totalNumbers; i++) {
 			System.out.print("#"+i+":");
 			int scannedNum = scan.nextInt();
 			if(scannedNum > max) {
@@ -22,28 +27,25 @@ public class ProcessingNumbers {
 			if(scannedNum < min) {
 				min = scannedNum;
 			}		
-			if(scannedNum % 2 == 0) {
+			if(scannedNum %2 ==0) {
+				sum+=scannedNum;
+				if(evenfound == false) {
+					evenmax = scannedNum;
+				}
 				evenfound = true;
-			}
-			if(evenfound = true) {
-				if(scannedNum %2 == 0) {
-					if(scannedNum > max) {
-						evenmax = scannedNum;
-					}
+				if(scannedNum > evenmax) {
+					evenmax = scannedNum;
 				}
-				if(scannedNum %2 ==0) {
-					sum+=scannedNum;
-				}
-			if(evenfound = false) {
-				sum = 0;
-			}
-			if(evenfound = false) {
-				evenmax = 0;
-			}
+			}	
 		}
-		System.out.println("max: "+max+" min: "+ min);
-		System.out.println("max of evens: " + evenmax);
-		System.out.println("Sum of even #s: "+sum);
+		if(evenfound == true) {
+			System.out.println("max: "+max+" min: "+ min);
+			System.out.println("max of evens: " + evenmax);
+			System.out.println("Sum of even #s: "+sum);
+		}else {
+			System.out.println("max: "+max+" min: "+ min);
+			System.out.println("max of evens: does not exist ");
+			System.out.println("Sum of even #s: does not exist");
 		}
 	}
 }
